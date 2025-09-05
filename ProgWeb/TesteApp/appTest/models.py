ca2b7e90a4f955a71793523877c586305c22b06b
Maria Eduarda Rodrigues Brandão <maria.r.brandao@dinfor.uniceub>
2025-09-05 10:52:33 -0300

    Arquivo modificado

--- ProgWeb/TesteApp/appTest/models.py
+++ ProgWeb/TesteApp/appTest/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+##Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'Produto: {self.nome}'
+
+'''class Produto(models.Model):
+    nome = models.CharField(max_length=100, unique=True) # nome do produto
+    descricao = models.TextField(blank=True, null=True) # descrição opcional
+    preco = models.DecimalField(max_digits=10, decimal_places=2) # 2 casas decimais
+    quantidade_estoque = models.PositiveIntegerField(default=0) # estoque inicial
+    data_criacao = models.DateTimeField(auto_now_add=True) # preenchido na criação
+    data_atualizacao = models.DateTimeField(auto_now=True) # atualizado sempre que salvar
+
+class Meta:
+    db_table = "produto" # nome da tabela no banco
+    ordering = ["nome"] # ordenação padrão (por nome)
+    verbose_name = "Produto" # nome singular para admin
+    verbose_name_plural = "Produtos" # nome plural para admin#
+
+def __str__(self):
+    return f"{self.nome} - R${self.preco}" '''
--- ProgWeb/TesteApp/appTest/views.py
+++ ProgWeb/TesteApp/appTest/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def index(request):
@@ -6,3 +7,7 @@ def index(request):
 
 def sobre(request):
     return render(request, 'appTest/sobre.html', {})
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'produtos/lista.html', {'produtos': produtos})

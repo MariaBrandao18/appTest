8178ef85a5757be3f5b8e34df1c4524d48b0a752
Maria Eduarda Rodrigues Brandão <maria.r.brandao@dinfor.uniceub>
2025-09-05 10:07:54 -0300

    Commit inicial

---
+++ ProgWeb/TesteApp/appTest/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'appTest/index.html', {})
+
+def sobre(request):
+    return render(request, 'appTest/sobre.html', {})
